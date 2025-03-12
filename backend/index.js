86f808bdfd5adc090907802fd06d29af8ed6754a
Mani-Amanda <135225894+Mani-Amanda@users.noreply.github.com>
2025-03-12 13:52:32 +0530

    update, delete book by id completed

--- backend/index.js
+++ backend/index.js
@@ -41,6 +41,72 @@ app.post('/books',async (request,response)=>{
 });
 
 //route for get all books from database
+app.get('/books',async(request,response)=>{
+    try{
+        const books=await Book.find({});
+        return response.status(200).json({
+            count: books.length,
+            data:books
+        }  
+        );
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({message:error.message});
+    }
+});
+
+//Route for  get one Book from database by id
+app.get('/books/:id',async(request,response)=>{
+    try{
+        const {id}=request.params;
+        const book=await Book.findById(id);
+        return response.status(200).json(book);
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({message:error.message});
+    }
+});
+
+//Route for Update a Book
+app.put('/books/:id',async(request,response)=>{
+    try{
+        if(
+            !request.body.title||
+            !request.body.author||
+            !request.body.publishYear
+        ){
+            return response.status(400).send({
+                message:'Send all required feilds: title author, pulishedYear',
+            });
+        }
+        const {id}=request.params;
+        const result= await Book.findByIdAndUpdate(id,request.body);
+        if(!result){
+            return response.status(404).json({message:'Book not found'});
+        }
+        return response.status(200).send({message:'Bokk updated succesfully'});
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({message:error.message});
+    }
+});
+
+//Route for delete a book
+app.delete('/books/:id',async(request,response)=>{
+    try{
+        const {id}=request.params;
+        const result =await Book.findByIdAndDelete(id);
+        if(!result){
+            return response.status(404).json({message:'Book not found'});
+        }
+
+        return response.status(200).send({message:'Book deleted succesfully'});
+
+    }catch(error){
+        console.log(error.message);
+        return response.status(500).send({message:error.message});
+    }
+})
 
 mongoose
     .connect(mongoDBURL)
